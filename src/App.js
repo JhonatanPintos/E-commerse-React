6261bc996b3be7e02e823a643b3a8b1647e7c793
jhonatan <pintos108@gmail.com>
2022-09-05 20:56:48 -0300

    correcion interno

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Navbar from "./Components/Navbar/Navbar"
-import { ItemListContainer } from "./ItemListContainer/ItemListContainer"
+import { ItemListContainer } from "./Container/ItemListContainer/ItemListContainer"
 const App = () =>{
 
     const mnjInicial = "Bienvenidos a esta E-Commerse"
